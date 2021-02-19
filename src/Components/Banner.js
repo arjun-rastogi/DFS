598a9237a6cfdbe54c82f0decd977c3a52695cb7
arjun-rastogi <arjun.rastogi2001@gmail.com>
2021-02-19 16:28:35 +0530

    Container Enabled

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Carousel, Button} from 'react-bootstrap';
+import {Carousel, Button, Container} from 'react-bootstrap';
 
 import image1 from '../Assets/banner/1.png'; 
 import image2 from '../Assets/banner/2.png';
@@ -8,6 +8,7 @@ import image3 from '../Assets/banner/3.png';
 
 function Banner() {
   return (
+    <Container>
 <Carousel>
   <Carousel.Item>
     <img
@@ -42,6 +43,7 @@ function Banner() {
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
+</Container>
   )
 }
 
